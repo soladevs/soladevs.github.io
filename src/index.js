3cb94e9d1fde27e1927c5f78cf651471c6e7430b
sola <dariuslewiss@hotmail.co.uk>
2024-02-07 14:58:20 +0000

    updates

--- src/index.js
+++ src/index.js
@@ -28,7 +28,9 @@ const App = () => {
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'minimal-ui');
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=device-width, initial-scale=1');
+//scroll to top on route change
+
 
 root.render(
   <HashRouter>
