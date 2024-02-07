cf32c367362a98e8abe92e4cf7faf0f357647893
sola <dariuslewiss@hotmail.co.uk>
2024-02-07 18:20:07 +0000

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,12 @@ import React, { useEffect, useState } from 'react';
 const Navbar = ({ openInitialState }) => {
   const [navEnabled, setNavEnabled] = useState(openInitialState);
 
-  useEffect(() => {
-    setNavEnabled(openInitialState);
-  }, [openInitialState]);
-
   const handleNavToggle = () => {
     setNavEnabled(!navEnabled);
   }
 
   return (
     <div className='navbar'>
-      <div />
-      {openInitialState ? 
-          <nav className='navigation'>
-          <ul>
-            <li><a href="/">home</a></li>
-            <li><a href="/#writing">writing</a></li>
-            <li><a href="/#about">about</a></li>
-            <li><a href="/#contact">contact</a></li>
-          </ul>
-        </nav>
-        :
         <div>
           <div className='nav-toggle' onClick={handleNavToggle}> 
             {navEnabled ? '>close' : '>menu'}
@@ -38,7 +23,6 @@ const Navbar = ({ openInitialState }) => {
         </ul>
       </nav>}
       </div>
-      }
     </div>
   );
 };
