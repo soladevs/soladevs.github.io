6981fb4b435edc2a75ea1ed336cd76222c12abab
sola <s@h.com>
2024-02-07 07:38:02 +0000

    update

--- src/About.js
+++ src/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import Footer from './components/Footer';
@@ -10,13 +10,28 @@ import './styles.scss';
 import './index.scss';
 import './article.scss';
 
+
+
+
 const About = () => {
+  const [content, setContent] = React.useState({});
+
+  // load about-me.md 
+  useEffect(() => {
+      fetch("/content/about-me.md")
+        .then(response => response.text())
+        .then(text => {
+          setContent(text);
+    }, []);
+  });
+
   return (
     <div>
       <Navbar openInitialState={false}/>
       <div className="main">
         <HeroSection />
       <section className="about-body">
+        <ReactMarkdown>{content}</ReactMarkdown>
       </section>
       </div>
     </div>
