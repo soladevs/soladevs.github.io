aa4a31639284763257f211b148f196cbb6b5ed18
sola <dariuslewiss@hotmail.co.uk>
2024-03-04 14:12:35 +0000

    schooling behaviour

--- src/UnityComponent.js
+++ src/UnityComponent.js
@@ -3,10 +3,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 const UnityComponent = () => {
     const { unityProvider } = useUnityContext({
-        loaderUrl: "./aquarium/b2nc.loader.js",
-        dataUrl: "./aquarium/b2nc.data",
-        frameworkUrl: "./aquarium/b2nc.framework.js",
-        codeUrl: "./aquarium/b2nc.wasm",
+        loaderUrl: "./aquarium/aquarium.loader.js",
+        dataUrl: "./aquarium/aquarium.data",
+        frameworkUrl: "./aquarium/aquarium.framework.js",
+        codeUrl: "./aquarium/aquarium.wasm",
     });
 
     return (
